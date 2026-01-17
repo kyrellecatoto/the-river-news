e2be0014789927dc76145a4880bc0fda75ac7b0d
SHEEN MILGER A. OCHAVO <90596472+shin-default123@users.noreply.github.com>
2026-01-17 23:04:38 +0800

    Sync google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 
@@ -16,7 +17,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
-      <body className={inter.className}>
+      <Head>
+        <meta name="title" content="The River" />
+        <meta
+          name="description"
+        />
+      </Head>
         {/* Google Analytics */}
         <Script
           strategy="lazyOnload"
@@ -38,7 +44,7 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6596634799308501"
      crossorigin="anonymous"
         />
-
+    <body className={inter.className}>
         {children}
         <Analytics />
       </body>
