540d70f6f5c729069d3ff928b9af3e37859af1af
SHEEN MILGER A. OCHAVO <90596472+shin-default123@users.noreply.github.com>
2026-01-17 23:13:49 +0800

    Debug issues

--- app/layout.js
+++ app/layout.js
@@ -3,48 +3,50 @@ import './globals.css';
 import Head from 'next/head';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
-
+ 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'The River',
-  description: 'Youth-run press, for Agusan & beyond',
-  icons: {
-    icon: '/favicon.ico',
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <Head>
+        <link rel="icon" href="/ikb.png" />
+
+        {/* Primary Meta Tags */}
         <meta name="title" content="The River" />
         <meta
           name="description"
         />
       </Head>
-        {/* Google Analytics */}
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `}
-        </Script>
 
-        {/* Google AdSense */}
-        <Script
-          strategy="afterInteractive"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6596634799308501"
-     crossorigin="anonymous"
-        />
-    <body className={inter.className}>
+      {/* ✅ Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
+      {/* ✅ Google AdSense (global, loaded once) */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6596634799308501"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
+      <body className={inter.className}>
         {children}
         <Analytics />
       </body>
